ce125b7f5252ea754dc74f875182e3c171bb6de9
theprakharsaxena <prakharsaxena5125@gmail.com>
2023-08-20 20:46:58 +0530

    adding some feature

---
+++ app/components/Editor.js
@@ -0,0 +1,31 @@
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css"; // Import the editor's styles
+
+export default function Editor({ value, onChange }) {
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      ["clean"],
+      [{ color: [] }, { background: [] }], // Add color and background color options
+    ],
+  };
+
+  return (
+    <div className="content">
+      <ReactQuill
+        value={value}
+        theme="snow"
+        onChange={onChange}
+        modules={modules}
+      />
+    </div>
+  );
+}
